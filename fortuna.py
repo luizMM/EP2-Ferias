f8f6b869ef6f71f8cff8f64a642ab5917f260f47
Pedro da Costa Alves <pedroca10@al.insper.edu.br>
2023-07-25 08:50:26 -0300

    Valida_questao

--- fortuna.py
+++ fortuna.py
@@ -7,3 +7,44 @@ def transforma_base(lista):
             dicionario[exr['nivel']] = []
         dicionario[exr['nivel']].append(exr)
     return dicionario
+
+def valida_questao(questao):
+    erros = {}
+    
+    chaves = ['titulo','nivel','opcoes','correta']
+    for c in chaves:
+        if c not in questao:
+            erros[c] = 'nao_encontrado'
+    
+    if len(questao) != 4:
+        erros['outro'] = 'numero_chaves_invalido'
+    
+    if 'titulo' in questao.keys():
+        if questao['titulo'].strip() == '':
+            erros['titulo'] = 'vazio'
+
+    if 'nivel' in questao.keys():
+        niveis = ['facil','medio','dificil']
+        if questao['nivel'] not in niveis:
+            erros['nivel'] = 'valor_errado' 
+
+    if 'opcoes' in questao.keys():
+        if len(questao['opcoes']) != 4:
+            erros['opcoes'] = 'tamanho_invalido'
+        else:
+    
+            letras = ['A','B','C','D']
+            for l in questao['opcoes'].keys():
+                if l not in questao['opcoes']:
+                    erros['opcoes'] = 'chave_invalida_ou_nao_encontrada'
+                else:
+                    if questao['opcoes'][l].strip() == '':
+                        if 'opcoes' not in erros:
+                            erros['opcoes'] = {}
+                        erros['opcoes'][l] = 'vazia'
+    
+    if 'correta' in questao.keys():
+        letras = ['A','B','C','D']
+        if questao['correta'] not in letras:
+            erros['correta'] = 'valor_errado'
+    return erros 
